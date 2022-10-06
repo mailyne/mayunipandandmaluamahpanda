4f1a796577dad8f885ade313aa1f6fe81e1f704a
mailyne <115188896+mailyne@users.noreply.github.com>
2022-10-06 17:38:41 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-
+let jog2 = 0
+let jog1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("go!!")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    jog2 += 1
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . . # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    jog1 += 1
+    basic.showLeds(`
+        . . . # .
+        . . # # .
+        . . . # .
+        . . . # .
+        . . # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
